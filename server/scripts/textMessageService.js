a416732341273f28c7a8ab361d5cbfbc4c53fe6d
German Gamboa <germangamboa95@gmail.com>
2018-06-03 00:39:27 -0400

    twilio text messageing function added

--- server/scripts/textMessageService.js
+++ server/scripts/textMessageService.js
@@ -1,16 +1,26 @@
 const  twilio = require('twilio');
 
-
-const accountSid = 'AC464b317faef364574c54db6881e35e91'; // Your Account SID from www.twilio.com/console
-const authToken =   process.env.twilio_key;   // Your Auth Token from www.twilio.com/console
+//  Twilio setup
+const accountSid = 'AC464b317faef364574c54db6881e35e91'; 
+const authToken =   process.env.twilio_key || '4863c240ebe8d276da0f7443e6d8c2d3'; 
 
 const  client = new twilio(accountSid, authToken);
 
-client.messages.create({
-    body: 'Hello from Node',
-    to: '+14077449871',  // Text this number
-    from: '+13212043039' // From a valid Twilio number
-})
-.then((message) => console.log(message.sid));
+// Send texts with message. Array wrapped in promise.all.
+const sendTexts = async (numbersArr, msg) => {
+    return Promise.all(numbersArr.map( async item => {
+        try {
+            return await client.messages.create({
+                body: msg,
+                to: `+1${item}`,  // Text this number
+                from: '+13212043039' // From a valid Twilio number
+            });
+            return await x;   
+        } catch (error) {
+            return error
+        }
+    }));
+}
 
 
+module.exports = sendTexts
